ce5034e836e1a566b39bdba8106050bb2a5ee98d
Bruno Palma Ávila <bpalma@LATINSHARE.COM>
2018-08-16 00:32:30 -0300

    don sergio sounds

---
+++ Animals.js
@@ -0,0 +1,10 @@
+export class Animals {
+    Perro : {
+        Archivos : ['./sounds/perro/Dog-barking-sound-big-dog.wav']
+    },
+    Gato : {
+        Archivos : ['./sounds/perro/Dog-barking-sound-big-dog.wav']
+    }
+}
+
+export = Animals;
--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 /**
- * Sample React Native App
+ * App sonidos 
  * https://github.com/facebook/react-native
  *
  * @format
@@ -7,20 +7,53 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Button, Platform, StyleSheet, Text, View} from 'react-native';
 const Realm = require('realm');
+export const sonidoPerro = require('./sounds/perro/Dog-barking-sound-big-dog.wav'); 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
   android:
     'Double tap R on your keyboard to reload,\n' +
     'Shake or press menu button for dev menu',
 });
+import Sound from 'react-native-sound';
+//import  {Animals}  from "./Animals";
+var soundPerro1 = require('./sounds/perro/Dog-barking-sound-big-dog.wav');
+var soundPerro2 = require('./sounds/perro/Dog-barking-sounds.wav');
+var soundPerro3 = require('./sounds/perro/Dog-barking-sound-sheeps-baa-background.wav');
+var soundGato1 = require('./sounds/gato/Cat-sound.wav');
+var soundSergio1 = require('./sounds/sergio/dia-de-mierda.mp3');
+var soundSergio2 = require('./sounds/sergio/mentiroso.mp3');
+var soundSergio3 = require('./sounds/sergio/mini-cuerno.mp3');
+var soundSergio4 = require('./sounds/sergio/mm-que-paja.mp3');
+var soundSergio5 = require('./sounds/sergio/puerco-putero.mp3');
+var soundSergio6 = require('./sounds/sergio/que-paja-esperar.mp3');
+var soundSergio7 = require('./sounds/sergio/rosario.mp3');
 
+const Animals = {//pico pal que lee
+  Perro : {
+    //Archivos : [{title: "1", sound : require('./sounds/perro/Dog-barking-sound-big-dog.wav')}, {title: "2", sound : require('./sounds/perro/Dog-barking-sound-big-dog.wav')}, {title: "3", sound : require('./sounds/perro/Dog-barking-sound-big-dog.wav')}]
+    Archivos : [{title: "3", sound : require('./sounds/perro/Dog-barking-sound-big-dog.wav')}]
+  },
+  Gato: {
+    Archivos : [soundGato1]
+  },
+  Sergio : {
+    Archivos : [{title: "1", sound : soundSergio1}, {title: "2", sound : soundSergio2}, {title: "3", sound : soundSergio3}, {title: "4", sound : soundSergio4}, {title: "5", sound : soundSergio5}, {title: "6", sound : soundSergio6}, {title: "7", sound : soundSergio7}]
+  }
+};
 type Props = {};
 export default class App extends Component<Props> {
   constructor(props) {
     super(props);
-    this.state = { realm: null };
+    this.state = { 
+      realm: null, 
+      TextButton : "Play", 
+      Sound  : null,
+      Animales : {},
+      CurrentKeyBtn: null
+    };
+    this.playSound = this.playSound.bind(this);
   }
 
   componentWillMount() {
@@ -35,20 +68,64 @@ export default class App extends Component<Props> {
   }
 
   render() {
-    const info = this.state.realm
+    var info = this.state.realm
       ? 'Number of dogs in this Realm: ' + this.state.realm.objects('Dog').length
       : 'Loading...';
-
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          {info}
+          Sonidos Don Sergio
         </Text>
+        {
+          Animals.Perro.Archivos.map((file, i) =>{
+            return (
+              <Button title={i == this.state.CurrentKeyBtn ? "Stop" : "Play"} key={i} onPress={() => {
+                return this.playSound(this, file, i);
+              }}/>
+            );
+
+          })
+
+        }
+       
       </View>
     );
   }
-}
+  playSound(component,item, key) {
+    var random = Math.floor(Math.random() * (Animals.Sergio.Archivos.length )) ;
+    var url = Animals.Sergio.Archivos[random].sound;
+    if(key == this.state.CurrentKeyBtn){
+      this.setState({ 
+        CurrentKeyBtn : null
+      });
+      if(component.state.Sound.isPlaying)
+        component.state.Sound.stop();
+    }else{
+      this.setState({ 
+        CurrentKeyBtn : key
+      });
+      const sound = new Sound(url, error => { 
+        component.setState({Sound : sound})
+        component.state.Sound.play((success) => {
+          if (success) {
+            this.setState({ 
+              CurrentKeyBtn : null
+            });
+          } 
+        });
+      });
 
+    }
+
+    
+
+    
+    
+    
+  }
+
+
+}
 const styles = StyleSheet.create({
   container: {
     flex: 1,
